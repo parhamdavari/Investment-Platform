ec9e02d9ffbf77a873d66cd961c139281712e5fa
parham <plavigne19@gmail.com>
2020-09-01 03:01:58 -0700

    set timer on buttons

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-const contractAddress = 'TGjqwrFZ8a2EUxbvbWsZv32YP9DpA6osCx';
+const contractAddress = 'THx5mtC9ZRj45QV3nSHpXesN3f8iugH3Mw';
 
 // TF2Qen1cXUu31dsYdKKSRYhigkh2QBdX8C
 const utils = {
